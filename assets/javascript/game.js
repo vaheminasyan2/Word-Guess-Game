c81081376cbc31241447ab410e66f95f4b9bed63
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2018-12-08 00:33:27 -0800

    changed a theme to geography and updated the style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,15 @@
 function playGame() {
 
 // Create varialbles and placeholder to update data on displayed on the screen
-    var wordChoices = ["jump", "relax", "africa", "lady", "centerfold", "shout"];
+    var wordChoices = ['afghanistan','albania','algeria','andorra','angola','argentina','armenia','australia','austria','azerbaijan','bahamas','bahrain','bangladesh','barbados','belarus','belgium','belize','bhutan','bolivia','bosnia','botswana','brazil','brunei','bulgaria','cambodia','canada','chad','chile','china','colombia','congo','costa rica','croatia','cuba','cyprus','czech republic','denmark','dominica','ecuador','egypt','estonia','ethiopia','fiji','finland','france','gambia','georgia','germany','ghana','great britain','greece','guatemala','guinea','haiti','honduras','hungary','iceland','india','indonesia','iran','iraq','israel','italy','jamaica','japan','jordan','kazakhstan','kenya','north korea','south korea','kuwait','kyrgyzstan','laos','latvia','lebanon','libya','liechtenstein','lithuania','luxembourg','macedonia','madagascar','malaysia','maldives','mali','malta','mauritania','mexico','moldova','monaco','mongolia','montenegro','morocco','mozambique','burma','namibia','nepal','new zealand','nicaragua','niger','nigeria','norway','oman','pakistan','panama','papua new guinea','paraguay','peru','philippines','poland','portugal','puerto rico','qatar','romania','russia','rwanda','samoa','saudi arabia','senegal','serbia','singapore','slovakia','slovenia','somalia','south africa','spain','sri lanka','sudan','sweden','switzerland','syria','tajikistan','tanzania','thailand','netherlands','togo','tunisia','turkey','turkmenistan','uganda','ukraine','united arab emirates','united states of america','uruguay','uzbekistan','venezuela','vietnam','yemen','zambia','zimbabwe'];
+    
     var wins = 0;
-
     var remainingGuesses;
     var lettersGuessed;
     var currentWord;
     var guessingWord;
     var userGuess; 
+    var word;
 
     var winsTag = document.getElementById ("wins-text");
     var remainingGuessesTag = document.getElementById ("remaining-guesses-text");
@@ -20,7 +21,7 @@ function playGame() {
 
 //Create function to select a word among available choices
     function selectWord() {
-        remainingGuesses = 10;
+        remainingGuesses = 15;
         lettersGuessed = [];
         guessingWord = [];
         remainingGuessesTag.textContent = remainingGuesses;
@@ -29,8 +30,9 @@ function playGame() {
 
         currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)]; 
         for (var i = 0; i<currentWord.length; i++) {
-            guessingWord [i]= " __ ";
-        };
+            if (currentWord[i] === " ") {guessingWord[i] = " "}
+            else {guessingWord[i] = " __ ";}
+        };        
         currentWordTag.textContent = guessingWord.join("");
         console.log(currentWord);
     };
